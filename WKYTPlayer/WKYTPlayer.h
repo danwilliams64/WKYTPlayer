3766d9325b23872f4762387bcc5ee2a2109a9aca
Dan Williams <dan@supapass.com>
2018-02-09 10:20:19 +0000

    Make WKYTPlayerView public

--- WKYTPlayer/WKYTPlayer.h
+++ WKYTPlayer/WKYTPlayer.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char WKYTPlayerVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <WKYTPlayer/PublicHeader.h>
 
 
+#import "WKYTPlayerView.h"
