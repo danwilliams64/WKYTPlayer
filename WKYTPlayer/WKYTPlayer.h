5f6b69d7d19987e383060f9105c036b06bdafc32
Dan Williams <dan@supapass.com>
2018-02-09 10:07:32 +0000

    Add Carthage support

---
+++ WKYTPlayer/WKYTPlayer.h
@@ -0,0 +1,19 @@
+//
+//  WKYTPlayer.h
+//  WKYTPlayer
+//
+//  Created by Dan on 09/02/2018.
+//  Copyright © 2018 Dan Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WKYTPlayer.
+FOUNDATION_EXPORT double WKYTPlayerVersionNumber;
+
+//! Project version string for WKYTPlayer.
+FOUNDATION_EXPORT const unsigned char WKYTPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WKYTPlayer/PublicHeader.h>
+
+
